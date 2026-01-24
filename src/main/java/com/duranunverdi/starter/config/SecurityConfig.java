d8fdcc506a5c74456e6ae5e437fe22033c44ab13
DuranUnverdi <duranunverdi1905@gmail.com>
2026-01-24 22:37:10 +0300

    AuthEntryPoint status code 403 convert 401 update

--- src/main/java/com/duranunverdi/starter/config/SecurityConfig.java
+++ src/main/java/com/duranunverdi/starter/config/SecurityConfig.java
@@ -29,12 +29,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authenticationProvider(authenticationProvider) // 🔥 ŞART
+                .authenticationProvider(authenticationProvider)
                 .exceptionHandling(ex ->
                         ex.authenticationEntryPoint(authEntryPoint)
                 )
